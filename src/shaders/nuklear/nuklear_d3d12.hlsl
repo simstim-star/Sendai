8c01d3d649b8cc0aab3fcd06aaf4f561264bd75a
simstim-star <raidemall01@gmail.com>
2026-03-13 20:39:52 -0300

    update nuklear

--- src/shaders/nuklear/nuklear_d3d12.hlsl
+++ src/shaders/nuklear/nuklear_d3d12.hlsl
@@ -15,39 +15,42 @@
 
 cbuffer buffer0 : register(b0)
 {
-    float4x4 ProjectionMatrix;
+  float4x4 ProjectionMatrix;
 };
-static uint texture_index : register(b1);
+cbuffer buffer1 : register(b1)
+{
+  uint texture_index;
+};
 
 sampler sampler0 : register(s0);
 Texture2D<float4> textures[] : register(t0);
 
 struct VS_INPUT
 {
-    float2 pos : POSITION;
-    float4 col : COLOR0;
-    float2 uv : TEXCOORD0;
+  float2 pos : POSITION;
+  float4 col : COLOR0;
+  float2 uv  : TEXCOORD0;
 };
 
 struct PS_INPUT
 {
-    float4 pos : SV_POSITION;
-    float4 col : COLOR0;
-    float2 uv : TEXCOORD0;
+  float4 pos : SV_POSITION;
+  float4 col : COLOR0;
+  float2 uv  : TEXCOORD0;
 };
 
 [RootSignature(NK_ROOTSIGNATURE)]
 PS_INPUT vs(VS_INPUT input)
 {
-    PS_INPUT output;
-    output.pos = mul(ProjectionMatrix, float4(input.pos.xy, 0.f, 1.f));
-    output.col = input.col;
-    output.uv = input.uv;
-    return output;
+  PS_INPUT output;
+  output.pos = mul(ProjectionMatrix, float4(input.pos.xy, 0.f, 1.f));
+  output.col = input.col;
+  output.uv  = input.uv;
+  return output;
 }
 
 [RootSignature(NK_ROOTSIGNATURE)]
 float4 ps(PS_INPUT input) : SV_Target
 {
-    return input.col * textures[texture_index].Sample(sampler0, input.uv);
+  return input.col * textures[texture_index].Sample(sampler0, input.uv);
 }
